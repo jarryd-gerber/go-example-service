b399ebce55f7603d57b2e63d32152fa180cbd42e
Jarryd Gerber <jarryd.gerber@gmail.com>
2022-02-14 22:08:55 +0000

    factory for transaction object

--- src/application/factory.go
+++ src/application/factory.go
@@ -3,21 +3,23 @@ package application
 import (
 	"github.com/jarryd-gerber/go-example-service/src/application/repository"
 	"github.com/jarryd-gerber/go-example-service/src/application/service"
+	"github.com/jarryd-gerber/go-example-service/src/domain"
 	"gorm.io/gorm"
 )
 
-func InitMachineRepo(database *gorm.DB) repository.Machine {
-	return repository.Machine{DB: database}
+func InitMachineRepo(db *gorm.DB) repository.Machine {
+	return repository.Machine{DB: db}
 }
 
-func InitCardRepo(database *gorm.DB) repository.Card {
-	return repository.Card{DB: database}
+func InitCardRepo(db *gorm.DB) repository.Card {
+	return repository.Card{DB: db}
 }
 
 func InitWithdrawal(db *gorm.DB) service.Withdrawal {
 	return service.Withdrawal{
 		CardRepo:    InitCardRepo(db),
 		MachineRepo: InitMachineRepo(db),
+		Transaction: domain.InitTransaction(),
 		DB:          db,
 	}
 }
--- src/domain/factory.go
+++ src/domain/factory.go
@@ -1,1 +1,9 @@
 package domain
+
+import (
+	"github.com/jarryd-gerber/go-example-service/src/domain/service"
+)
+
+func InitTransaction() service.Transaction {
+	return service.Transaction{}
+}
